3a84a9f0797bf9984e0433dc2e7b8c98bc4e6355
gibbbeh <45526369+gibbbeh@users.noreply.github.com>
2023-12-04 17:16:08 -0500

    Data from input fields now stored

--- components/LoginPage.jsx
+++ components/LoginPage.jsx
@@ -4,6 +4,17 @@ import './login-style.css'; // Import your CSS file here
 function LoginPage() {
   const [isActive, setIsActive] = useState(false);
 
+  const [inputValues, setInputValues] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setInputValues({ ...inputValues, [id]: value });
+  };
+
   const handleRegisterClick = () => {
     setIsActive(true);
   };
@@ -12,26 +23,67 @@ function LoginPage() {
     setIsActive(false);
   };
 
+  const handleSignUpClick = (e) => {
+    e.preventDefault();
+    console.log('Sign Up button clicked. Valued:', inputValues);
+
+  }
+  const handleSignInClick = (e) => {
+    e.preventDefault();
+    console.log('Sign In button clicked. Valued:', inputValues);
+
+  }
+
   return (
     <div className={`container ${isActive ? 'active' : ''}`} id="container">
       <div className="form-container sign-up">
         <form>
           <h1>Create Account</h1>
           <span>Use your MC email for registration</span>
-          <input type="text" placeholder="Name" />
-          <input type="email" placeholder="Manhattan College Email" />
-          <input type="password" placeholder="Password" />
-          <button>Sign Up</button>
+          <input
+            type="text"
+            id="name"
+            placeholder="Name"
+            onChange={handleChange}
+            value={inputValues.name}
+          />
+          <input
+            type="email"
+            id="email"
+            placeholder="Manhattan College Email"
+            onChange={handleChange}
+            value={inputValues.email}
+          />
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            onChange={handleChange}
+            value={inputValues.password}
+          />
+          <button onClick={handleSignUpClick}>Sign Up</button>
         </form>
       </div>
       <div className="form-container sign-in">
         <form>
           <h1>Sign In</h1>
           <span>Use your MC email password</span>
-          <input type="email" placeholder="Manhattan College Email" />
-          <input type="password" placeholder="Password" />
+          <input
+            type="email"
+            id="email"
+            placeholder="Manhattan College Email"
+            onChange={handleChange}
+            value={inputValues.email}
+          />
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            onChange={handleChange}
+            value={inputValues.password}
+          />
           <a href="#">Forget Your Password?</a>
-          <button>Sign In</button>
+          <button onClick={handleSignInClick}>Sign In</button>
         </form>
       </div>
       <div className="toggle-container">
